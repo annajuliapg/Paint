dcd71a9d71237eda3137ae121a89910242178330
ANNA JÚLIA PESSONI GOUVÊA <anna.jpg@puccampinas.edu.br>
2020-03-13 08:23:19 -0300

    Projeto inicial

---
+++ src/Circulo.java
@@ -0,0 +1,84 @@
+import java.awt.*;
+import java.util.*;
+ 
+public class Circulo extends Figura
+{
+    protected Ponto centro;
+    protected int raio;
+    
+    public Circulo (int x, int y, int raio)
+    {
+        this (x, y, raio, Color.BLACK);
+    }
+	
+    public Circulo (int x, int y, int raio, Color cor)
+    {
+        super(cor);
+
+        this.centro = new Ponto (x,y,cor);
+        this.raio = raio;
+    }
+
+    public Circulo (String s)
+    {
+        StringTokenizer quebrador = new StringTokenizer(s,":");
+
+        quebrador.nextToken();
+
+        int   x  = Integer.parseInt(quebrador.nextToken());
+        int   y  = Integer.parseInt(quebrador.nextToken());
+
+        int   raio  = Integer.parseInt(quebrador.nextToken());
+
+        Color cor = new Color (Integer.parseInt(quebrador.nextToken()),  // R
+                               Integer.parseInt(quebrador.nextToken()),  // G
+                               Integer.parseInt(quebrador.nextToken())); // B
+
+        this.centro  = new Ponto (x,y,cor);
+        this.raio  = raio;
+        this.cor = cor;
+    }
+
+    public void setCentro (int x, int y)
+    {
+        this.centro = new Ponto (x,y,this.getCor());
+    }
+
+    public void setRaio (int raio)
+    {
+        this.raio = raio;
+    }
+
+    public Ponto getCentro ()
+    {
+        return this.centro;
+    }
+
+    public int getRaio ()
+    {
+        return this.raio;
+    }
+
+    public void torneSeVisivel (Graphics g)
+    {
+        g.setColor(this.cor);
+        g.drawOval(this.centro.getX(), this.centro.getY(),   // centro
+                   2*this.raio, 2*this.raio);               // diametro
+    }
+
+    public String toString()
+    {
+        return "c:" +
+               this.centro.getX() +
+               ":" +
+               this.centro.getY() +
+               ":" +
+               this.raio +
+               ":" +
+               this.getCor().getRed() +
+               ":" +
+               this.getCor().getGreen() +
+               ":" +
+               this.getCor().getBlue();
+    }
+}
